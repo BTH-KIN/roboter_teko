fddf3b85b8d78871234e0637c9eb3b1b33cc25ac
Van Potros <van.potros@edu.teko.ch>
2022-05-23 15:58:53 +0200

    drivercontrol

--- Motordriver.py
+++ Motordriver.py
@@ -12,13 +12,14 @@ class driver:
         self.in4 = g0.OutputDevice(6)
         self.en2 = g0.PWMLED(13)
 
-    def motorvorwarts(self):
+    def motorvorwarts(self,speed,wait):
         self.in1.on()
         self.in2.off()
-        self.en1.value = 0.5
+        self.en1.value = speed
         self.en1.toggle()
         
-        sleep(5)
+
+        sleep(wait)
 
     def linksdrehen(self):
         self.en1.on()
@@ -55,18 +56,16 @@ class driver:
         self.in3.off()
         self.in4.on()
         sleep(5)          
-
-
+    
+  
+    def drivecontrol(self,richtung,speed, wait):
+       if richtung in "vorwarts":
+          self.motorvorwarts(speed,wait) 
 
 if __name__ == '__main__':
-    motor1 = driver()
-    print("motorvorwarts")
-    motor1.motorvorwarts()
-   # print("linksdrehen")
-   # motor1.linksdrehen()
-   # print("ruckwarts")
-   # motor1.ruckwarts()
-   # motor1.vorwarts()
-   # motor1.linksdrehen()
+    roboter=driver()
+    roboter.drivecontrol("vorwarts",0.5,10)
+   
+ 
 
 
