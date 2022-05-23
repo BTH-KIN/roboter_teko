494a247e949753f77870b224cb171d163ec7e110
Giuseppe La Martina <giuseppe.lamartina@edu.teko.ch>
2022-05-23 16:02:22 +0200

    Face reconize on videoStream.py

--- camera/camera.py
+++ camera/camera.py
@@ -1,72 +1,80 @@
+#!/usr/bin/python3
+
+# ------------------------------------------------------------------------------
+# Find geometric shaped objects in the image using houghCircles().
+# ------------------------------------------------------------------------------
+# automaticdai
+# YF Robotics Labrotary
+# Instagram: yfrobotics
+# Twitter: @yfrobotics
+# Website: https://www.yfrl.org
+# ------------------------------------------------------------------------------
+# Reference: 
+# - https://www.pyimagesearch.com/2014/07/21/detecting-circles-images-using-opencv-hough-circles/
+# ------------------------------------------------------------------------------
+
 import cv2
-#Pretrained classes in the model
-ObjectsNames = {0: 'background',
-              1: 'person', 2: 'bicycle', 3: 'car', 4: 'motorcycle', 5: 'airplane', 6: 'bus',
-              7: 'train', 8: 'truck', 9: 'boat', 10: 'traffic light', 11: 'fire hydrant',
-              13: 'stop sign', 14: 'parking meter', 15: 'bench', 16: 'bird', 17: 'cat',
-              18: 'dog', 19: 'horse', 20: 'sheep', 21: 'cow', 22: 'elephant', 23: 'bear',
-              24: 'zebra', 25: 'giraffe', 27: 'backpack', 28: 'umbrella', 31: 'handbag',
-              32: 'tie', 33: 'suitcase', 34: 'frisbee', 35: 'skis', 36: 'snowboard',
-              37: 'sports ball', 38: 'kite', 39: 'baseball bat', 40: 'baseball glove',
-              41: 'skateboard', 42: 'surfboard', 43: 'tennis racket', 44: 'bottle',
-              46: 'wine glass', 47: 'cup', 48: 'fork', 49: 'knife', 50: 'spoon',
-              51: 'bowl', 52: 'banana', 53: 'apple', 54: 'sandwich', 55: 'orange',
-              56: 'broccoli', 57: 'carrot', 58: 'hot dog', 59: 'pizza', 60: 'donut',
-              61: 'cake', 62: 'chair', 63: 'couch', 64: 'potted plant', 65: 'bed',
-              67: 'dining table', 70: 'toilet', 72: 'tv', 73: 'laptop', 74: 'mouse',
-              75: 'remote', 76: 'keyboard', 77: 'cell phone', 78: 'microwave', 79: 'oven',
-              80: 'toaster', 81: 'sink', 82: 'refrigerator', 84: 'book', 85: 'clock',
-              86: 'vase', 87: 'scissors', 88: 'teddy bear', 89: 'hair drier', 90: 'toothbrush'}
+import numpy as np
+
+CAMERA_DEVICE_ID = 0
+IMAGE_WIDTH = 320
+IMAGE_HEIGHT = 240
 
 
-#Loading model
-model = cv2.dnn.readNetFromTensorflow('/home/pi/workspace/roboter_teko/camera/models/frozen_inference_graph.pb',
-                                      '/home/pi/workspace/roboter_teko/camera/models/ssd_mobilenet_v2_coco_2018_03_29.pbtxt')
-
-#Loading image
-image = cv2.imread("/home/pi/workspace/roboter_teko/camera/dogndapple.jpg")
-#Getting height and shape of image
-image_height, image_width, _ = image.shape
-
-
-blob=cv2.dnn.blobFromImage(image, size=(299, 299), swapRB=True)
-print("First Blob: {}".format(blob.shape))
+def isset(v):
+    try:
+        type (eval(v))
+    except:
+        return 0
+    else:
+        return 1
 
 
-#set blob as input to model
-model.setInput(cv2.dnn.blobFromImage(image, size=(299, 299), swapRB=True))
+if __name__ == "__main__":
+    try:
+        # create video capture
+        cap = cv2.VideoCapture(CAMERA_DEVICE_ID)
 
+        # set resolution to 320x240 to reduce latency 
+        cap.set(3, IMAGE_WIDTH)
+        cap.set(4, IMAGE_HEIGHT)
 
-def id_Object_class(class_id, classes):
-    for key_id, class_name in classes.items():
-        if class_id == key_id:
-            return class_name
+        while True:
+            # Read the frames frome a camera
+            _, frame = cap.read()
+            frame = cv2.blur(frame,(3,3))
 
+            # Or get it from a JPEG
+            # frame = cv2.imread('frame0010.jpg', 1)
+        
+            # convert the image into gray color
+            output = frame.copy()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-output = model.forward()
-#print(output[0,0,:,:])
-#print(output)
-for detection in output[0, 0, :, :]:
-    confidence = detection[2]       #confidence for occuring a class
-    #print(detection[2])
-    if confidence > .35:            #threshold
-        class_id = detection[1]
-        #print(detection[1])
-        class_name=id_Object_class(class_id,ObjectsNames)       #calling id_Object_class function
-        print(str(str(class_id) + " " + str(detection[2])  + " " + class_name))
-        box_x = detection[3] * image_width
-        box_y = detection[4] * image_height
-        box_width = detection[5] * image_width
-        box_height = detection[6] * image_height
-        cv2.rectangle(image, (int(box_x), int(box_y)), (int(box_width),
-                            int(box_height)), (23, 230, 210), thickness=1) #drawing rectangle
-        cv2.putText(image,class_name ,(int(box_x), int(box_y+.05*image_height)),
-        cv2.FONT_HERSHEY_SIMPLEX,(.005*image_width),(0, 0, 255)) #text
+            # detect circles in the image
+            circles = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+            # circles = cv2.HOUGH_STANDARD(gray, cv2.HOUGH_GRADIENT, 1.2, 100)
+            
+            # ensure at least some circles were found
+            if circles is not None:
+                # convert the (x, y) coordinates and radius of the circles to integers
+                circles = np.round(circles[0, :]).astype("int")
+                # loop over the (x, y) coordinates and radius of the circles
+                for (x, y, r) in circles:
+                    # draw the circle in the output image, then draw a rectangle
+                    # corresponding to the center of the circle
+                    cv2.circle(output, (x, y), r, (0, 255, 0), 4)
+                    cv2.rectangle(output, (x - 5, y - 5), (x + 5, y + 5), (0, 128, 255), -1)
+            
+            # show the output image
+            cv2.imshow("frame", np.hstack([frame, output]))
 
-
-
-
-cv2.imshow('image', image)
-# cv2.imwrite("image_box_text.jpg",image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+            # if key pressed is 'Esc' then exit the loop
+            if cv2.waitKey(33) == 27:
+                break
+    except Exception as e:
+        print(e)
+    finally:
+        # Clean up and exit the program
+        cv2.destroyAllWindows()
+        cap.release()
