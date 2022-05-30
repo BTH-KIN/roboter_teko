a63b3338ed1cb8b4ced789387e615ddc04ee598c
Van Potros <van.potros@edu.teko.ch>
2022-05-30 14:22:11 +0200

    neue Funktionen

--- Motordriver.py
+++ Motordriver.py
@@ -1,5 +1,6 @@
 #/usr/bin/python
 
+from email.utils import encode_rfc2231
 from time import sleep
 import gpiozero as g0
 
@@ -12,60 +13,113 @@ class driver:
         self.in4 = g0.OutputDevice(6)
         self.en2 = g0.PWMLED(13)
 
-    def motorvorwarts(self,speed,wait):
+    
+
+    def linksdrehen(self,wait,speed):
+        self.en1.value = speed
+        self.en2.value = speed
+        self.en1.toggle()
+        self.en2.toggle()
+        self.en1.on()
+        self.en2.on()
         self.in1.on()
         self.in2.off()
-        self.en1.value = speed
-        self.en1.toggle()
-        
+        self.in3.on()
+        self.in4.off()
 
         sleep(wait)
-
-    def linksdrehen(self):
-        self.en1.on()
-        self.en2.on()
-        self.in1.on()
-        self.in2.off()
-        self.in3.on()
-        self.in4.off()
-        sleep(5)
-
-    def ruckwarts(self):
-        self.en1.on()
-        self.en2.on()
-        self.in1.on()
-        self.in2.off()
-        self.in3.off()
-        self.in4.on()
-        sleep(5)
-
-    def vorwarts(self):
-        self.en1.on()
-        self.en2.on()
-        self.in1.off()
-        self.in2.on()
-        self.in3.on()
-        self.in4.off()
-        sleep(5)
-
-    def rechsdrehen(self):
+        
+        
+    def rechtsdrehen(self,speed,wait):
+        self.en1.value = speed
+        self.en2.value = speed
+        self.en1.toggle()
+        self.en2.toggle()
         self.en1.on()
         self.en2.on()
         self.in1.off()
         self.in2.on()
         self.in3.off()
         self.in4.on()
-        sleep(5)          
+        sleep(wait) 
+        self.en1.off()
+        self.en2.off() 
+
+    def ruckwarts(self,speed,wait):
+        self.en1.value = speed
+        self.en2.value = speed
+        self.en1.toggle()
+        self.en2.toggle()
+        self.in1.on()
+        self.in2.off()
+        self.in3.off()
+        self.in4.on()
+        sleep(wait)
+        self.en1.off()
+        self.en2.off() 
+
+
+    def vorwarts(self,speed,wait):
+        self.en1.value = speed
+        self.en2.value = speed
+        self.en1.toggle()
+        self.en2.toggle()
+        self.in1.off()
+        self.in2.on()
+        self.in3.on()
+        self.in4.off()
+        sleep(wait)
+        self.en1.off()
+        self.en2.off() 
+
+    def motoraus(self):
+        self.en1.off()
+        self.en2.off()
+
     
   
-    def drivecontrol(self,richtung,speed, wait):
-       if richtung in "vorwarts":
-          self.motorvorwarts(speed,wait) 
+    def drivecontrol(self,richtung,speed,wait):
+
+        if richtung in "vorwarts":
+          self.vorwarts(speed,wait) 
+
+        if richtung in "ruckwarts":
+            self.ruckwarts(speed,wait) 
+
+        if richtung in "rechts":
+          self.rechtsdrehen(speed,wait) 
+        
+        if richtung in "links":
+          self.linksdrehen(speed,wait) 
+
+       
 
 if __name__ == '__main__':
     roboter=driver()
-    roboter.drivecontrol("vorwarts",0.5,10)
+
+    speed = 0.1
+    
+
+    timer = 0.6
+    timel = 0.6
+    
+
+    roboter.drivecontrol("rechts",speed,timer)
+    roboter.motoraus()
+    sleep(1)
+
+    roboter.drivecontrol("links",speed,timel)
+    roboter.motoraus()
+    sleep(1)
+    
+    roboter.drivecontrol("vorwarts",speed,2)
+    roboter.motoraus()
+
+    roboter.drivecontrol("ruckwarts",speed,2)
+    roboter.motoraus()
    
+    roboter.drivecontrol("vorwarts",speed,3)
+    roboter.motoraus()
  
 
 
